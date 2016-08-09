cfc036cbda3e77137c6913742486d1025b85d3eb
VanDalkvist <i_van@list.ru>
2016-08-09 16:11:07 +0300

    fixed bugs with stopped elevators; refactoring; more logs;

--- src/solution.js
+++ src/solution.js
@@ -1,31 +1,31 @@
-// dependencies
+{
+    init: function _init(elevators, floors) {
 
-// exports
+        var sharedQueue = [];
+        var upQueue = [];
+        var downQueue = [];
 
-module.exports = {
-    init: _init,
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
-};
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed", _handleUpButtonPressOnFloor.bind(null, floor));
 
-// initialization
+            floor.on("down_button_pressed", _handleDownButtonPressOnFloor.bind(null, floor));
+        });
 
-// private methods
+        elevators.forEach(function (elevator) {
+            elevator.on("idle", _findFloorToGoTo.bind(null, elevator));
 
-function _init(elevators, floors) {
+            elevator.on("stopped_at_floor", _stoppedAtFloorHandler.bind(null, elevator));
 
-    var generalQueue = [];
-    var upQueue = [];
-    var downQueue = [];
+            elevator.on('floor_button_pressed', _floorButtonPressHandler.bind(null, elevator));
 
-    floors.forEach(function (floor) {
+            elevator.on("passing_floor", _passingFloorHandler.bind(null, elevator));
+        });
 
-        floor.on("up_button_pressed", function() {
-
+        function _handleUpButtonPressOnFloor(floor) {
             var floorNum = floor.floorNum();
 
-            //debugger;
+            console.log('up button has been pressed at ' + floorNum + ' floor');
+
             var stopped = _getStoppedElevator();
             if (stopped !== null) {
                 stopped.goToFloor(floorNum, true);
@@ -33,21 +33,21 @@ function _init(elevators, floors) {
                 return;
             }
 
-            var matched = elevators.filter(function (elevator) {
-                return elevator.destinationDirection() === "up"
-                    && elevator.currentFloor() < floorNum
-                    && elevator.loadFactor() <= 0.7;
-            });
+            // var matched = elevators.filter(function (elevator) {
+            //     return elevator.destinationDirection() === "up"
+            //         && elevator.currentFloor() < floorNum
+            //         && elevator.loadFactor() <= 0.7;
+            // });
+            //
+            // if (matched && matched.length > 0) {
+            //     matched[0].goToFloor(floorNum);
+            //     console.log(floorNum + ' floor added to elevator queue (up)');
+            //     return;
+            // }
 
-            if (matched && matched.length > 0) {
-                matched[0].goToFloor(floorNum);
-                console.log(floorNum + ' floor added to elevator queue (up)');
-                return;
-            }
-
-            if (generalQueue.indexOf(floorNum) < 0) {
-                generalQueue.push(floorNum);
-                console.log(floorNum + ' floor pushed to generalQueue');
+            if (sharedQueue.indexOf(floorNum) < 0) {
+                sharedQueue.push(floorNum);
+                console.log(floorNum + ' floor pushed to sharedQueue');
             }
             if (upQueue.indexOf(floorNum) < 0) {
                 upQueue.push(floorNum);
@@ -62,12 +62,11 @@ function _init(elevators, floors) {
             //    elevator.checkDestinationQueue();
             // elevator.goToFloor(floor.floorNum(), true);
             //}
-        });
+        }
 
-        floor.on("down_button_pressed", function() {
+        function _handleDownButtonPressOnFloor(floor) {
             var floorNum = floor.floorNum();
-
-            //debugger;
+            console.log('down button has been pressed at ' + floorNum + ' floor');
 
             var stopped = _getStoppedElevator();
             if (stopped !== null) {
@@ -76,61 +75,83 @@ function _init(elevators, floors) {
                 return;
             }
 
-            var matched = elevators.filter(function (elevator) {
-                return elevator.destinationDirection() === "down"
-                    && elevator.currentFloor() > floorNum
-                    && elevator.loadFactor() <= 0.7;
-            });
+            // var matched = elevators.filter(function (elevator) {
+            //     return elevator.destinationDirection() === "down"
+            //         && elevator.currentFloor() > floorNum
+            //         && elevator.loadFactor() <= 0.7;
+            // });
+            //
+            // if (matched && matched.length > 0) {
+            //     matched[0].goToFloor(floorNum);
+            //     console.log(floorNum + ' floor added to elevator queue (down)');
+            //     return;
+            // }
 
-            if (matched && matched.length > 0) {
-                matched[0].goToFloor(floorNum);
-                console.log(floorNum + ' floor added to elevator queue (down)');
-                return;
-            }
-
-            if (generalQueue.indexOf(floorNum) < 0) {
-                generalQueue.push(floorNum);
-                console.log(floorNum + ' floor pushed to generalQueue');
+            if (sharedQueue.indexOf(floorNum) < 0) {
+                sharedQueue.push(floorNum);
+                console.log(floorNum + ' floor pushed to sharedQueue');
             }
             if (downQueue.indexOf(floorNum) < 0) {
                 downQueue.push(floorNum);
                 downQueue = downQueue.sort();
                 console.log(floorNum + ' floor pushed to downQueue');
             }
-        });
-    });
+        }
 
-    elevators.forEach(function (elevator) {
-        elevator.on("idle", function() {
-
+        function _findFloorToGoTo(elevator) {
             var identity = _getFloorComparer(elevator);
-            var nearPressedFloor = _getNearPressedFloor(elevator, generalQueue, identity);
+            console.log('idle on ' + elevator.currentFloor() + ' floor. Searching near floor...');
+            console.log('idle: up queue: ' + upQueue.join(', ') + '; down queue: ' + downQueue.join(', ') + '; shared queue: ' + sharedQueue.join(', '));
+            var nearPressedFloor = _getNearPressedFloor(elevator, sharedQueue, identity);
 
             if (nearPressedFloor === undefined || nearPressedFloor === null) {
-                console.log('pop from generalQueue');
-            } else {
-                var index = generalQueue.indexOf(nearPressedFloor);
-                generalQueue.splice(index, 1);
-                elevator.goToFloor(nearPressedFloor);
-                console.log('go to near floor');
+                console.log('near floor was not found.');
+                return;
             }
-        });
 
-        elevator.on("stopped_at_floor", function(floorNum) {
+            _syncQueues(nearPressedFloor);
+
+            console.log('idle: up queue: ' + upQueue.join(', ') + '; down queue: ' + downQueue.join(', ') + '; shared queue: ' + sharedQueue.join(', '));
+            elevator.goToFloor(nearPressedFloor);
+            console.log('go to near floor - ' + nearPressedFloor);
+        }
+
+        function _stoppedAtFloorHandler(elevator, floorNum) {
             var identity = _getFloorComparer(elevator);
+
+            _syncQueues(floorNum);
+
+            console.log('elevator stopped at ' + elevator.currentFloor() + ' floor. Searching near floor...');
             var nearPressedFloor = _getNearPressedFloor(elevator, elevator.getPressedFloors(), identity);
 
             if (nearPressedFloor === undefined || nearPressedFloor === null) {
                 console.log('floor_button_pressed: nothing to do');
                 return;
             }
-            //debugger;
+
+            _syncQueues(nearPressedFloor);
+
             elevator.goToFloor(nearPressedFloor, true);
             console.log('go to nearPressedFloor (stopped at floor) - ' + nearPressedFloor);
-        });
+        }
 
-        elevator.on('floor_button_pressed', function (floorNum) {
-            console.log('floor button has been pressed - ' + floorNum);
+        function _syncQueues(floor) {
+            var foundIndex = sharedQueue.indexOf(floor);
+            sharedQueue.splice(foundIndex, 1);
+
+            foundIndex = upQueue.indexOf(floor);
+            if (foundIndex > -1) {
+                upQueue.splice(foundIndex, 1);
+            }
+
+            foundIndex = downQueue.indexOf(floor);
+            if (foundIndex > -1) {
+                downQueue.splice(foundIndex, 1);
+            }
+        }
+
+        function _floorButtonPressHandler(elevator, floorNum) {
+            console.log('floor button has been pressed - ' + floorNum + '. Searching near elevator...');
 
             var identity = _getFloorComparer(elevator);
             var nearPressedFloor = _getNearPressedFloor(elevator, elevator.getPressedFloors(), identity);
@@ -139,31 +160,31 @@ function _init(elevators, floors) {
                 console.log('floor_button_pressed: nothing to do');
                 return;
             }
-            //debugger;
+
+            _syncQueues(nearPressedFloor);
+
             elevator.goToFloor(nearPressedFloor, true);
             console.log('go to nearPressedFloor (floor_button_pressed)- ' + nearPressedFloor);
-        });
+        }
 
-        elevator.on("passing_floor", function(floorNum, direction) {
+        function _passingFloorHandler(elevator, floorNum, direction) {
             console.log('passing_floor: ' + floorNum + ', ' + direction);
-            var queue = direction === "up" ? upQueue : downQueue;
+            console.log('up queue: ' + upQueue.join(', ') + '; down queue: ' + downQueue.join(', ') + '; shared queue: ' + sharedQueue.join(', '));
 
             var pressedFloors = elevator.getPressedFloors();
-
             if (pressedFloors.indexOf(floorNum) > -1) {
                 elevator.goToFloor(floorNum, true);
                 console.log('go to one of pressed floors - ' + floorNum);
-                var foundIndex = queue.indexOf(floorNum);
-                if (foundIndex > -1) {
-                    queue.splice(foundIndex, 1);
-                }
+                _syncQueues(floorNum);
+                return;
             }
 
-            //debugger
+            var queue = direction === "up" ? upQueue : downQueue;
             var foundIndex = queue.indexOf(floorNum);
             if (foundIndex > -1) {
                 if (elevator.loadFactor() <= 0.75) {
-                    queue.splice(foundIndex, 1);
+                    _syncQueues(floorNum);
+
                     console.log('calculated min - stopping on ' + floorNum);
                     elevator.goToFloor(floorNum, true);
                 }
@@ -174,83 +195,87 @@ function _init(elevators, floors) {
             else {
                 console.log('could not calculate min');
             }
-        });
-    });
-
-    function _getStoppedElevator() {
-        var matched = elevators.filter(function (elevator) {
-            return elevator.destinationDirection() === "stopped";
-        });
-
-        if (matched && matched.length === 1) {
-            console.log('getting first stopped elevator');
-            return matched[0];
         }
 
-        if (matched && matched.length > 1) {
-            // todo: optimize
+        function _getStoppedElevator() {
+            var matched = elevators.filter(function (elevator) {
+                return elevator.destinationDirection() === "stopped" && elevator.loadFactor() === 0;
+            });
 
-            console.log('getting first (from many) stopped elevator');
-            return matched[0];
-        }
+            if (matched && matched.length === 1) {
+                console.log('getting first stopped elevator');
+                return matched[0];
+            }
 
-        return null;
-    }
+            if (matched && matched.length > 1) {
+                // todo: optimize
 
-    function _getFloorComparer(elevator) {
-        if (elevator.goingUpIndicator()) {
-            return function (floor) {
-                return floor > elevator.currentFloor();
-            };
-        }
-        else if (elevator.goingDownIndicator()) {
-            return function (floor) {
-                return floor < elevator.currentFloor();
-            };
-        }
-        return function (floor) {
-            return floor;
-        };
-    }
+                console.log('getting first (from many) stopped elevator');
+                return matched[0];
+            }
 
-    function _getNearPressedFloor(elevator, floorList, identity) {
-        var pressed = floorList;
-
-        if (pressed.length === 0) {
             return null;
         }
 
-        if (pressed.length === 1) {
-            return pressed[0];
-        }
-
-        var floors = pressed.filter(identity);
-
-        var sorted = floors.sort();
-
-        var indexOfMin = _getMin(sorted, function (floor) {
-            return Math.abs(floor - elevator.currentFloor());
-        });
-
-        var choice = sorted[indexOfMin];
-        console.log('choice - ' + choice);
-        return choice;
-    }
-
-    function _getMin(arr, predicate) {
-        var foundIndex = -1;
-        var minValue = -1;
-
-        for ( var i = 0; i < arr.length; i++ ) {
-            var el = arr[i];
-
-            var calc = predicate(el);
-            if (calc < minValue) {
-                minValue = calc;
-                foundIndex = i;
+        function _getFloorComparer(elevator) {
+            if (elevator.goingUpIndicator()) {
+                return function (floor) {
+                    return floor > elevator.currentFloor();
+                };
             }
+            else if (elevator.goingDownIndicator()) {
+                return function (floor) {
+                    return floor < elevator.currentFloor();
+                };
+            }
+            return function (floor) {
+                return floor !== undefined && floor !== null;
+            };
         }
 
-        return i;
+        function _getNearPressedFloor(elevator, floorList, identity) {
+            var pressed = floorList;
+
+            if (pressed.length === 0) {
+                return null;
+            }
+
+            if (pressed.length === 1) {
+                return pressed[0];
+            }
+
+            var floors = pressed.filter(identity);
+
+            var sorted = floors.sort();
+
+            var indexOfMin = _getMin(sorted, function (floor) {
+                return Math.abs(floor - elevator.currentFloor());
+            });
+
+            var choice = sorted[indexOfMin];
+            console.log(sorted.join(', ') + ' [' + indexOfMin + '] - ' + choice);
+            return choice;
+        }
+
+        function _getMin(arr, predicate) {
+            var foundIndex = -1;
+            var minValue = Number.MAX_VALUE;
+
+            for (var i = 0; i < arr.length; i++) {
+                var el = arr[i];
+
+                var calc = predicate(el);
+                if (calc < minValue) {
+                    minValue = calc;
+                    foundIndex = i;
+                }
+            }
+
+            return foundIndex;
+        }
+    }
+,
+    update: function (dt, elevators, floors) {
+        // We normally don't need to do anything here
     }
 }
